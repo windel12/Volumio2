512ce06f81fc476f662f383ad420bfee6b3ca34f
Ubuntu <ubuntu@volumio-test.canonistack>
2018-11-07 16:25:07 +0000

    init mpd folders

---
+++ snap/local/scripts/mpd_folders_setups.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo "Creating Playlists folder"
+PLAYLIST_FOLDER="$SNAP_COMMON/playlists"
+if [ ! -d "$PLAYLIST_FOLDER" ]; then
+  mkdir -p "$PLAYLIST_FOLDER"
+fi
+
+echo "Creating Music folder"
+MUSIC_FOLDER="$SNAP_COMMON/music"
+if [ ! -d "$MUSIC_FOLDER" ]; then
+  mkdir -p "$MUSIC_FOLDER"
+fi
+
+echo "Creating Database File"
+DB_FILE="$SNAP_COMMON/tag_cache"
+if [ ! -f "$DB_FILE" ]; then
+  touch "$DB_FILE"
+fi
+
+
+echo "Starting mpd"
+usr/bin/mpd $SNAP/etc/mpd.conf
